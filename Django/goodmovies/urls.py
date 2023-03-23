647d4b853a15960e6c95280de6335e3977593cbb
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2023-03-24 00:45:30 +0100

    Update Django

--- Django/goodmovies/urls.py
+++ Django/goodmovies/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movies import views  # NOWE
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('hello/', views.hello_world),  # NOWE
 ]
---
+++ Django/movies/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def hello_world(request):
+#     return HttpResponse("Witaj świecie!")
+    return render(request, template_name="hello.html")  # NOWE
