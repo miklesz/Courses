80a6d33c61b3393c1fc6f67f4699ca07c90d2cb4
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2023-03-30 21:43:51 +0200

    Update

--- Django/movies/models.py
+++ Django/movies/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    short_description = models.TextField()
+    published_at = models.DateTimeField()
--- Django/movies/views.py
+++ Django/movies/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from movies.models import *
+from datetime import datetime  # NOWE
 
+
+# # Create your views here.
+# def hello_world(request):
+# #     return HttpResponse("Witaj świecie!")
+#     return render(request, template_name="hello.html")  # NOWE
 
 # Create your views here.
 def hello_world(request):
-#     return HttpResponse("Witaj świecie!")
-    return render(request, template_name="hello.html")  # NOWE
+    our_context = {"time": datetime.now()}  # NOWE
+    return render(request, template_name="hello.html", context=our_context)  # NOWE
 
 def list_movies(request):
     movies = Movie.objects.all()
