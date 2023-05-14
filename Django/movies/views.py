92d616c147970d5bb5cb53b02c67e9c2034f5c06
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2023-05-14 17:11:25 +0200

    Update Django 3

--- Django/movies/views.py
+++ Django/movies/views.py
@@ -1,6 +1,20 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime  # NOWE
+
+# Create your views here.
+# def hello_world(request):
+#     return HttpResponse("Witaj świecie!")
+
+# Create your views here.
+# def hello_world(request):
+#     return render(request, template_name="hello.html")  # NOWE
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse("Witaj świecie!")
+    our_context = {"time": datetime.now()}  # NOWE
+    return render(
+        request,
+        template_name="hello.html",
+        context=our_context
+    )  # NOWE
