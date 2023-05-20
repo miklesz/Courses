94c6aa5001543b007c910a33dd171d4cbea3c7a9
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2023-05-20 17:18:39 +0200

    Update Django Model

--- Django/movies/admin.py
+++ Django/movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from movies.models import Movie  # NOWE
+
+admin.site.register(Movie)  # NOWE
--- Django/movies/models.py
+++ Django/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    short_description = models.TextField()
+    published_at = models.DateTimeField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.title
