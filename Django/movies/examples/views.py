a5b04a5d200d0c4860eccab9df49faf2e1f6d4fa
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2023-05-20 18:41:34 +0200

    Update Django

---
+++ Django/movies/examples/views.py
@@ -0,0 +1,30 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime  # NOWE
+
+# Create your views here.
+# def hello_world(request):
+#     return HttpResponse("Witaj świecie!")
+
+# Create your views here.
+# def hello_world(request):
+#     return render(request, template_name="hello.html")  # NOWE
+
+# Create your views here.
+def hello_world(request):
+    our_context = {"time": datetime.now()}  # NOWE
+    return render(
+        request,
+        template_name="hello.html",
+        context=our_context
+    )  # NOWE
+
+from movies.models import Movie
+
+def list_movies(request):
+    movies = Movie.objects.all()
+    return render(request, template_name="examples/movie_list.html", context={"movies": movies})
+
+def movie_details(request):
+    details = Movie.objects.all()[0]
+    return render(request, template_name="examples/details.html", context={"details": details})
