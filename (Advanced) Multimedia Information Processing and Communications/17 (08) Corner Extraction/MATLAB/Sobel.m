8518f07dfd7f5f201e4fc3dc3eb39b3142d12b7e
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2024-09-29 00:13:52 +0200

    Clean up repository by removing obsolete files

--- (Advanced) Multimedia Information Processing and Communications/17 (08) Corner Extraction/MATLAB/Sobel.m
+++
@@ -1,5 +0,0 @@
-I = imread('pilka.png'); %przyk³adowy obraz w odcieniach szaroœci
-[I2,I3]=imgradient(I, 'Sobel'); 
-imshowpair(I,I2,'montage'); %Natê¿enie krawêdzi
-figure(2);
-imshowpair(I,I3,'montage'); %Kierunkowoœæ krawêdzi
